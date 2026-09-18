65ee7ac600c7b2c5d1e7081cf95bb51150c4c7ab
Oriol Nieto <oriolnieto2006@gmail.com>
2026-09-18 15:47:46 +0200

    S01: posada en marxa

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular-entorns-2627');
+  protected readonly title = signal('angular-entorns-2627'); // modificació random
 }
